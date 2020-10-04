25418d8cd718e6b1f8126e41a8d80f63714906c1
smooker <atvservice@mail.bg>
2020-10-04 17:18:29 +0300

    sending is working

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -58,10 +58,8 @@ __IO ITStatus Uart1Ready = RESET;
 uint8_t aTxBufferPos = 0;
 uint8_t aTxBuffer[256];
 
-uint8_t aRxBufferPos = 0;
-uint8_t aRxBuffer[BUFFERSIZE];
-
-uint8_t needResponse = 0;
+//uint8_t aRxBufferPos = 0;
+//uint8_t aRxBuffer[BUFFERSIZE];
 
 struct MBCmds {
    uint8_t  cmdlen;
@@ -69,8 +67,33 @@ struct MBCmds {
 };
 
 struct MBCmds mbcmds[10] = {
-    {1, {0x52}},                      //TEST TAMAGAWA
+    {1, {0x53}},                      //TEST TAMAGAWA
 };
+
+struct tmgw_struc {
+   uint8_t cf;
+   uint8_t sf;
+   uint8_t d0;
+   uint8_t d1;
+   uint8_t d2;
+   uint8_t d3;
+   uint8_t d4;
+   uint8_t d5;
+   uint8_t d6;
+   uint8_t d7;
+   uint8_t crc;
+   uint8_t len;
+   uint8_t received;       //13
+};
+
+struct tmgw_struc2 {
+    struct tmgw_struc rx;
+    struct tmgw_struc tx;
+};
+
+struct tmgw_struc2 tmgw;
+
+int txcnt2 = 0;
 
 /* USER CODE END PV */
 
@@ -114,7 +137,8 @@ void HAL_UART_RxIdleCallback(UART_HandleTypeDef* huart)
         /* Determine how many items of data have been received */
 
         rxXferCount = huart->RxXferSize - __HAL_DMA_GET_COUNTER(hdma);
-        aRxBufferPos = rxXferCount;
+//        aRxBufferPos = rxXferCount;
+        tmgw.rx.len = rxXferCount;
 
         huart->RxXferCount = 0;
 
@@ -129,45 +153,44 @@ void HAL_UART_RxIdleCallback(UART_HandleTypeDef* huart)
             huart->gState = HAL_UART_STATE_READY;
         }
 
-//        processTMGWMessage(&huart1, (uint8_t *)aRxBuffer, rxXferCount);
-//        BKPT;
+//        printf("CF2:0x%02x %02d\r\n", tmgw.rx.cf, tmgw.rx.len);
 
-        printf("RX1:%02x\r\n", aRxBuffer[0]);
-        needResponse = 1;
+        tmgw.rx.received = 1;
 
-//        if (aRxBuffer[0] == 0x52) {
-//            sendmbcmd(3);
-//        }
-
-        HAL_UART_Receive_DMA(&huart1, (uint8_t *)aRxBuffer, 256);
         return;
     }
 
     return;
 }
 
+void startReceive()
+{
+    memset(&tmgw.rx, 0, sizeof(tmgw.rx));
+    HAL_UART_Receive_DMA(&huart1, (uint8_t *)&tmgw.rx, sizeof(tmgw.rx));
+}
+
 void HAL_UART_RxCpltCallback(UART_HandleTypeDef *huart)
 {
     if (huart->Instance == USART1)
     {
-        printf("RX2:%02x\r\n", aRxBuffer[0]);
-        if(HAL_UART_Receive_DMA(&huart1, (uint8_t *)aRxBuffer, 256) != HAL_OK)
+        printf("RX2:%02x\r\n", tmgw.rx.cf);
+        if(HAL_UART_Receive_DMA(&huart1, (uint8_t *)&tmgw.rx, sizeof (tmgw.rx)) != HAL_OK)
         {
             Error_Handler();
         }
-        needResponse = 1;
+        tmgw.rx.received = 1;
     }
 }
 
 void HAL_UART_TxCpltCallback(UART_HandleTypeDef *huart)
 {
-    BKPT;
     if (huart->Instance == USART1)
     {
         /* Set transmission flag: transfer complete */
         Uart1Ready = SET;
         HAL_GPIO_WritePin(DE_GPIO_Port, DE_Pin, GPIO_PIN_RESET);
     }
+    txcnt2++;
 }
 
 
@@ -250,6 +273,53 @@ void ITM_Init(void)
     ITM->TER = 0xFFFFFFFF; // Enable all stimulus ports
 }
 
+unsigned crc8x_simple(unsigned crc, void const *mem, size_t len) {
+    unsigned char const *data = mem;
+    if (data == NULL)
+        return 0xff;
+    while (len--) {
+        crc ^= *data++;
+        for (unsigned k = 0; k < 8; k++)
+            crc = crc & 0x80 ? (crc << 1) ^ 0x31 : crc << 1;
+    }
+    crc &= 0xff;
+    return crc;
+}
+
+static unsigned char const crc8x_table[] = {
+    0x00, 0x31, 0x62, 0x53, 0xc4, 0xf5, 0xa6, 0x97, 0xb9, 0x88, 0xdb, 0xea, 0x7d,
+    0x4c, 0x1f, 0x2e, 0x43, 0x72, 0x21, 0x10, 0x87, 0xb6, 0xe5, 0xd4, 0xfa, 0xcb,
+    0x98, 0xa9, 0x3e, 0x0f, 0x5c, 0x6d, 0x86, 0xb7, 0xe4, 0xd5, 0x42, 0x73, 0x20,
+    0x11, 0x3f, 0x0e, 0x5d, 0x6c, 0xfb, 0xca, 0x99, 0xa8, 0xc5, 0xf4, 0xa7, 0x96,
+    0x01, 0x30, 0x63, 0x52, 0x7c, 0x4d, 0x1e, 0x2f, 0xb8, 0x89, 0xda, 0xeb, 0x3d,
+    0x0c, 0x5f, 0x6e, 0xf9, 0xc8, 0x9b, 0xaa, 0x84, 0xb5, 0xe6, 0xd7, 0x40, 0x71,
+    0x22, 0x13, 0x7e, 0x4f, 0x1c, 0x2d, 0xba, 0x8b, 0xd8, 0xe9, 0xc7, 0xf6, 0xa5,
+    0x94, 0x03, 0x32, 0x61, 0x50, 0xbb, 0x8a, 0xd9, 0xe8, 0x7f, 0x4e, 0x1d, 0x2c,
+    0x02, 0x33, 0x60, 0x51, 0xc6, 0xf7, 0xa4, 0x95, 0xf8, 0xc9, 0x9a, 0xab, 0x3c,
+    0x0d, 0x5e, 0x6f, 0x41, 0x70, 0x23, 0x12, 0x85, 0xb4, 0xe7, 0xd6, 0x7a, 0x4b,
+    0x18, 0x29, 0xbe, 0x8f, 0xdc, 0xed, 0xc3, 0xf2, 0xa1, 0x90, 0x07, 0x36, 0x65,
+    0x54, 0x39, 0x08, 0x5b, 0x6a, 0xfd, 0xcc, 0x9f, 0xae, 0x80, 0xb1, 0xe2, 0xd3,
+    0x44, 0x75, 0x26, 0x17, 0xfc, 0xcd, 0x9e, 0xaf, 0x38, 0x09, 0x5a, 0x6b, 0x45,
+    0x74, 0x27, 0x16, 0x81, 0xb0, 0xe3, 0xd2, 0xbf, 0x8e, 0xdd, 0xec, 0x7b, 0x4a,
+    0x19, 0x28, 0x06, 0x37, 0x64, 0x55, 0xc2, 0xf3, 0xa0, 0x91, 0x47, 0x76, 0x25,
+    0x14, 0x83, 0xb2, 0xe1, 0xd0, 0xfe, 0xcf, 0x9c, 0xad, 0x3a, 0x0b, 0x58, 0x69,
+    0x04, 0x35, 0x66, 0x57, 0xc0, 0xf1, 0xa2, 0x93, 0xbd, 0x8c, 0xdf, 0xee, 0x79,
+    0x48, 0x1b, 0x2a, 0xc1, 0xf0, 0xa3, 0x92, 0x05, 0x34, 0x67, 0x56, 0x78, 0x49,
+    0x1a, 0x2b, 0xbc, 0x8d, 0xde, 0xef, 0x82, 0xb3, 0xe0, 0xd1, 0x46, 0x77, 0x24,
+    0x15, 0x3b, 0x0a, 0x59, 0x68, 0xff, 0xce, 0x9d, 0xac};
+
+unsigned crc8x_fast(unsigned crc, void const *mem, size_t len) {
+    unsigned char const *data = mem;
+    if (data == NULL)
+        return 0xff;
+    crc &= 0xff;
+    while (len--)
+        crc = crc8x_table[crc ^ *data++];
+    return crc;
+}
+
+
+
 //printf on ITM
 int __write(int file, char *ptr, int len)
 {
@@ -262,21 +332,23 @@ int __write(int file, char *ptr, int len)
   return len;
 }
 
-void sendmbcmd(uint8_t cmdno) {
-
+void sendmbcmd(uint8_t cmdno)
+{
     uint8_t len = mbcmds[cmdno].cmdlen;
 
-    uint8_t cnt;
-    for(cnt=0;cnt<len;cnt++) {
-        aTxBuffer[cnt]=mbcmds[cmdno].cmd[cnt];
-    }   //fixme. to be done with memcpy
+    tmgw.tx.cf = 0x52;
+    tmgw.tx.sf = 0x00;
+    tmgw.tx.d0 = 0xff;
+    tmgw.tx.d1 = 0x55;
+    tmgw.tx.d2 = 0xaa;
+    tmgw.tx.len = 5;
 
     HAL_StatusTypeDef stat = HAL_ERROR;
     HAL_GPIO_WritePin(DE_GPIO_Port, DE_Pin, GPIO_PIN_SET);
 
     if ((huart1.gState == HAL_UART_STATE_READY) || (huart1.gState == HAL_UART_STATE_BUSY_RX)) {
 
-        stat = HAL_UART_Transmit_DMA(&huart1, (uint8_t*)&aTxBuffer, len);         // W/O
+        stat = HAL_UART_Transmit_DMA(&huart1, (uint8_t*)&tmgw.tx, tmgw.tx.len);         // W/O
 
         if(stat != HAL_OK)
         {
@@ -285,7 +357,7 @@ void sendmbcmd(uint8_t cmdno) {
 
     }
 
-    /*## Wait for the end of the transfer ###################################*/
+    /*## Wait for the end of the transfer ################################### zashtooo ?*/
     while (Uart1Ready != SET)
     {
     }
@@ -335,7 +407,7 @@ int main(void)
 
 //  HAL_Delay(200);
 
-  printf("BOOT\r\n");
+  printf("BOOT\n");
 
   /* USER CODE END 2 */
 
@@ -343,13 +415,23 @@ int main(void)
   /* USER CODE BEGIN WHILE */
 
   int cnt = 0;
+  int txcnt = 0;
 
   while (1)
   {
-      printf("%04d\r\n", cnt++);
-//      if ( (needResponse == 1) && aRxBuffer[0] == 0x52 ) {
+      //tuka ako niamame zabaviane se uebava... da vidim kyde tochno
+      if ( (cnt % 13067) == 1) {
+          printf("%04d\n", cnt);
+      }
+      cnt++;
+//      printf("DE:%04x\n", tmgw.rx.received);
+//      printf("DF:%04x\n", tmgw.rx.cf);
+//      BKPT;
+      if ( (tmgw.rx.received == 1) && (tmgw.rx.cf == 0x52) ) {
           sendmbcmd(0);
-//      }
+          printf("TX:%04d %04d\r\n", txcnt++, txcnt2);
+      }
+      startReceive();
     /* USER CODE END WHILE */
 
     /* USER CODE BEGIN 3 */
@@ -486,9 +568,9 @@ static void MX_USART1_UART_Init(void)
 
   /* USER CODE END USART1_Init 1 */
   huart1.Instance = USART1;
-//  huart1.Init.BaudRate = 2500000;
-  huart1.Init.BaudRate = 115200;
-  huart1.Init.WordLength = UART_WORDLENGTH_9B;
+  huart1.Init.BaudRate = 2500000;
+//  huart1.Init.BaudRate = 115200;
+  huart1.Init.WordLength = UART_WORDLENGTH_8B;
   huart1.Init.StopBits = UART_STOPBITS_1;
   huart1.Init.Parity = UART_PARITY_NONE;
   huart1.Init.Mode = UART_MODE_TX_RX;
@@ -505,10 +587,7 @@ static void MX_USART1_UART_Init(void)
   __HAL_UART_ENABLE_IT(&huart1, UART_IT_IDLE);
 
   //Activate UART DMA for receive
-  if(HAL_UART_Receive_DMA(&huart1, (uint8_t *)&aRxBuffer, 256) != HAL_OK)
-  {
-    Error_Handler();
-  }
+  startReceive();
   /* USER CODE END USART1_Init 2 */
 
 }
@@ -556,7 +635,7 @@ static void MX_GPIO_Init(void)
   /*Configure GPIO pin : DE_Pin */
   GPIO_InitStruct.Pin = DE_Pin;
   GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
-  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Pull = GPIO_PULLUP;
   GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_HIGH;
   HAL_GPIO_Init(DE_GPIO_Port, &GPIO_InitStruct);
 
@@ -586,10 +665,10 @@ void Error_Handler(void)
 
 //  printf("HAL ERROR!!!");
 
-//  while (1) {
-//    HAL_GPIO_TogglePin(LED0_GPIO_Port, LED0_Pin);
-//    HAL_Delay(100);
-//  }
+  while (1) {
+    HAL_GPIO_TogglePin(LED0_GPIO_Port, LED0_Pin);
+    HAL_Delay(100);
+  }
 
   /* USER CODE END Error_Handler_Debug */
 }
